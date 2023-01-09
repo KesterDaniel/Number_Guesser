3f15e3492b4d145a6cb9195431054cb2127bce86
KesterDaniel <kesterdan17@gmail.com>
2023-01-09 17:06:27 +0100

    pick a random number btw 1 and 100 and ask for difficulty level

---
+++ app.py
@@ -0,0 +1,10 @@
+# Number guesser 
+import random
+
+# random number between 1 an 100
+magic_number = random.randint(1, 100)
+
+print("I'm thinking of a number between 1 and 100")
+
+# ask for difficulty level
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
